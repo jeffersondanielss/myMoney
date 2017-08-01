f4cd4c6b4aa7b0a15f8d0e02104d450010527588
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-01 15:30:59 -0300

    funcionalidade: Sidebar component adicionado

---
+++ frontend/src/common/template/menu.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export default props => (
+  <ul className="sidebar-menu">
+    
+  </ul>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,9 +1,12 @@
 import '../common/template/dependencies'
 import React from 'react';
+
 import Header from '../common/template/header'
+import Sidebar from '../common/template/sideBar'
 
 export default props => (
   <div className="wrapper">
     <Header></Header>
+    <Sidebar></Sidebar>
   </div>
 )
