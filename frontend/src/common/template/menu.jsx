201549e0650b5e81fdac9e2e81b24eb17e3c2d2e
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-01 15:51:23 -0300

    funcionalidade: MenuItem e MenuTree component adicionado

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import MenuItem from './menuItem'
+import MenuTree from './menuTree'
 
 
 export default props => (
   <ul className="sidebar-menu">
-    
+    <MenuItem path="#/" label="Dashboard" icon="dashboard"></MenuItem>
+    <MenuTree label="Cadastro" icon="edit">
+      <MenuItem path="#billingCycles" label="Cilcos de pagamentos" icon="usd"></MenuItem>
+    </MenuTree>
   </ul>
 )
