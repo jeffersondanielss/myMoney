bd4eeb707eee610945567ea8e3205d85605c4999
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-01 18:02:50 -0300

    funcionalidade: Adição do componente Content e ContentHeader

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Content from '../common/template/content'
+import ContentHeader from '../common/template/contentHeader'
 
-export default props => (
-  <div>
-    <h1>Dashboard</h1>
-  </div>
-)
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <ContentHeader title="Dashboard" small="Versão 1.0" />
+        <Content>
+          Dashboard
+        </Content>
+
+      </div>
+    )
+  }
+}
+
+export default Dashboard
