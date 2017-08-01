1df9eb4346de6dae8a83376b0e7502af68e55d17
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-01 15:55:07 -0300

    funcionalidade: Footer component adicionado

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+  <footer className="main-footer">
+    <string>
+      Copyright &copy; 2017
+      <a href="#" target="_blank"> JDSS</a>
+    </string>
+  </footer>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,10 +3,17 @@ import React from 'react';
 
 import Header from '../common/template/header'
 import Sidebar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
 
 export default props => (
   <div className="wrapper">
     <Header></Header>
     <Sidebar></Sidebar>
+    
+    <div className="content-wrapper">
+      <h1>content</h1>
+    </div>
+
+    <Footer></Footer>
   </div>
 )
