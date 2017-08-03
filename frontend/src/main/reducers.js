10a78ebdafc583601fdc13a097f4f7b0f00da31f
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-03 13:44:43 -0300

    feat: adição do dashboardReducer

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux'
+import DashboardReducer from '../dashboard/dashboardReducer'
 
 const rootReducer = combineReducers({
-  dashboard: () => ({summary:{ credit: 100, debt: 50 } })
+  dashboard: DashboardReducer
 })
 
 
