f026fe9fa260d4fd89f1e047a7c1a3a0bb0de76f
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-04 16:02:09 -0300

    feat: add actions

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+import { getSummary } from './dashboardActions'
 import Content from '../common/template/content'
 import ContentHeader from '../common/template/contentHeader'
 import ValueBox from '../common/widget/valueBox'
 import Row from '../common/layout/row'
 
 class Dashboard extends Component {
+
+  componentWillMount(){
+    this.props.getSummary();
+  }
+
   render() {
 
     const { credit, debt } = this.props.summary
@@ -27,5 +35,5 @@ class Dashboard extends Component {
 }
 
 const mapStateToProps = state => ({ summary: state.dashboard.summary })
-
-export default connect(mapStateToProps)(Dashboard)
+const mapDispatchToPorps = dispatch => bindActionCreators({getSummary}, dispatch)
+export default connect(mapStateToProps, mapDispatchToPorps)(Dashboard)
