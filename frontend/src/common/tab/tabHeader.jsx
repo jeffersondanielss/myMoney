50867ddd798de327960cf64d3e9303935e987727
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-08 14:16:46 -0300

    feat: add redux in tabHeader component

--- frontend/src/common/tab/tabHeader.jsx
+++ frontend/src/common/tab/tabHeader.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { selectTab } from './tabActions'
 
 class TabHeader extends Component {
 
   render() {
+    const selected = this.props.tab.selected === this.props.target
     return (
-      <li>
+      <li className={ selected ? 'active' : '' }>
         <a
           href="javascript:;"
           data-toggle='tab'
-          data-target={ this.props.tabs }>
+          data-target={ this.props.tabs }
+          onClick={ () => this.props.selectTab( this.props.target ) }>
           <i className={ `fa fa-${this.props.icon}` }></i> { this.props.label }
         </a>
       </li>
@@ -17,4 +22,7 @@ class TabHeader extends Component {
 
 }
 
-export default TabHeader
+const mapStateToProps = state => ({ tab: state.tab })
+const mapDispatchToProps = dispatch => bindActionCreators( { selectTab }, dispatch )
+
+export default connect(mapStateToProps, mapDispatchToProps)(TabHeader)
