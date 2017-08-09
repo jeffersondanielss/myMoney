7153684f1ebe5da056e0391d664c543ea9116851
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-09 13:37:32 -0300

    feat: add redux to component

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import Tabs from '../common/tab/tabs'
@@ -6,10 +9,14 @@ import TabsHeader from '../common/tab/tabsHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
+import { selectTab, showTabs } from '../common/tab/tabActions'
 class BillingCycle extends Component {
+  componentWillMount() {
+    this.props.selectTab('tabList')
+    this.props.showTabs('tabList', 'tabCreate')
+  }
 
   render() {
-
     return (
       <div>
         <ContentHeader title="Ciclo de pagamento" small="Cadastro" />
@@ -34,4 +41,5 @@ class BillingCycle extends Component {
   }
 }
 
-export default BillingCycle
+const mapDispatchToProps = dispatch => bindActionCreators( { selectTab, showTabs }, dispatch )
+export default connect(null, mapDispatchToProps)(BillingCycle)
