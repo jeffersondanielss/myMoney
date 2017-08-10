cd704f8b4c11a0107e83d8cb95f751906e71e8e6
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-10 19:12:34 -0300

    feat: dispatch actions after redux form submit

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
+import { reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:3003/api'
 
@@ -13,15 +15,19 @@ export function getList() {
 }
 
 export function create(values) {
-  axios.post(`${BASE_URL}/billingcycles`, values)
-    .then( resp => {
-      toastr.success('Sucesso', 'Operação realizada com sucesso.' )
-    })
-    .catch( respError => {
-      respError.response.data.errors.forEach( error => toastr.error('Erro', error ) )
-    })
-
-  return {
-    type: 'TEMP'
+  return dispatch => {  
+    axios.post(`${BASE_URL}/billingcycles`, values)
+      .then( resp => {
+        toastr.success('Sucesso', 'Operação realizada com sucesso.' )
+        dispatch([
+          resetForm('billingCycleForm'),
+          getList(),
+          selectTab('tabList'),
+          showTabs('tabList', 'tabCreate')
+        ])
+      })
+      .catch( respError => {
+        respError.response.data.errors.forEach( error => toastr.error('Erro', error ) )
+      })
   }
 }
