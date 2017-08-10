7073521d1fbc62d5a18cfc8cc2def7aaeac082a3
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-10 19:00:16 -0300

    feat: add error and success messages

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
 
 const BASE_URL = 'http://localhost:3003/api'
 
@@ -13,6 +14,13 @@ export function getList() {
 
 export function create(values) {
   axios.post(`${BASE_URL}/billingcycles`, values)
+    .then( resp => {
+      toastr.success('Sucesso', 'Operação realizada com sucesso.' )
+    })
+    .catch( respError => {
+      respError.response.data.errors.forEach( error => toastr.error('Erro', error ) )
+    })
+
   return {
     type: 'TEMP'
   }
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -5,6 +5,7 @@ import Header from '../common/template/header'
 import Sidebar from '../common/template/sideBar'
 import Footer from '../common/template/footer'
 import Routes from './routes'
+import Messages from '../common/msg/messages'
 
 export default props => (
   <div className='wrapper'>
@@ -16,5 +17,6 @@ export default props => (
     </div>
 
     <Footer></Footer>
+    <Messages />
   </div>
 )
