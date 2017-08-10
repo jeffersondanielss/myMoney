d9fd4eae5644a5986f884b1c6fcb266f64d088c4
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-10 19:27:06 -0300

    refactor: Change style input for adminLTE

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
+import labelAndInput from '../common/form/labelAndInput'
 
 class BillingCycleForm extends Component {
   render() {
@@ -8,9 +9,9 @@ class BillingCycleForm extends Component {
     return (
       <form action='' role='form' onSubmit={ handleSubmit }>
         <div className='box-body'>
-          <Field name='name' component='input' />
-          <Field name='month' component='input' />
-          <Field name='year' component='input' />
+          <Field name='name' component={ labelAndInput } label='Nome' cols='12 4' placeholder='Informe o nome' />
+          <Field name='month' component={ labelAndInput } label='Mes' cols='12 4' placeholder='Informe o mês' type='number' />
+          <Field name='year' component={ labelAndInput } label='Ano' cols='12 4' placeholder='Informe o ano' type='number' />
         </div>
         <div className='box-footer'>
           <button type='submit' className='btn btn-primary'>Submit</button>
