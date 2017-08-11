3a6c4cd46a2e561cba2525aef2c8e15385a29b3b
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-11 16:19:15 -0300

    feat: add and clone credits

--- frontend/src/billingCycle/creditList.jsx
+++ frontend/src/billingCycle/creditList.jsx
@@ -1,18 +1,36 @@
 import React, { Component } from 'react'
+import { Field, arrayInsert } from 'redux-form'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
 import Grid from '../common/layout/grid'
-import { Field } from 'redux-form'
 import Input from '../common/form/input'
 
 class CreditList extends Component {
+  add( index, item = {} ) {
+    if(!this.props.readOnly) {
+      this.props.arrayInsert('billingCycleForm', 'credits', index, item)
+    }
+  }
+
   renderRows() {
     const list = this.props.list || []
+
 
     return (
       list.map( (item, index) => (
         <tr key={ index }>
           <td><Field name={`credits[${index}].name`} component={ Input } placeholder='Informe o nome' readOnly={ this.props.readOnly } /></td>
           <td><Field name={`credits[${index}].value`} component={ Input } placeholder='Informe o valor' readOnly={ this.props.readOnly } /></td>
-          <td></td>
+          <td className='table-actions'>
+            <button type='button' className="btn btn-success" onClick={ () => this.add( index + 1 ) }>
+              <i className="fa fa-plus"></i>
+            </button>
+
+            <button type='button' className="btn btn-warning" onClick={ () => this.add( index + 1, item ) }>
+              <i className="fa fa-clone"></i>
+            </button>
+          </td>
         </tr>
       ))
     )
@@ -43,4 +61,5 @@ class CreditList extends Component {
   }
 }
 
-export default CreditList
+const mapDispatchToProps = dispatch => bindActionCreators( { arrayInsert }, dispatch )
+export default connect(null, mapDispatchToProps)(CreditList)
