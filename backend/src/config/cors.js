5cbf66e31ec921ab4dd7ae488dabf3cbf0e54d16
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-14 16:45:58 -0300

    feat: add middlewares

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATH, DELETE')
-  res.header('Access-Control-Allow-Headers', 'Origin, x-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATH, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Origin, x-Requested-With, Content-Type, Accept, Authorization')
   next()
 }
