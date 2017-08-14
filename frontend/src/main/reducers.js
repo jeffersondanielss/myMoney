aa88686ae21bf6026293f1afa037ea21a3727011
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-14 17:14:25 -0300

    feat: auth reducer and actions

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
 import { reducer as ToastrReducer } from 'react-redux-toastr'
 
+import AuthReducer from '../auth/authReducer'
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
 import billingCycleReducer from '../billingCycle/billingCycleReducer'
@@ -11,7 +12,8 @@ const rootReducer = combineReducers({
   tab: TabReducer,
   billingCycle: billingCycleReducer,
   form: formReducer,
-  toastr: ToastrReducer
+  toastr: ToastrReducer,
+  auth: AuthReducer
 })
 
 
