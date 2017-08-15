e345729ef0ebc56ed45bd5ad0e39738066c6d798
Jefferson Daniel <jdsilva@sp.r7.com>
2017-08-15 13:32:40 -0300

    feat: add auth front-end

--- frontend/src/common/template/navbar.jsx
+++ frontend/src/common/template/navbar.jsx
@@ -15,26 +15,27 @@ class Navbar extends Component {
 
   render() {
     const { name, email } = this.props.user
+    
     return (
       <div className="navbar-custom-menu">
         <ul className="nav navbar-nav">
 
-          <li onMouseLeave={() => this.changeOpen()} className={`dropdown user user-menu ${this.state.open ? 'open' : ''}`}>
-            <a href="javascript:;" onClick={() => this.changeOpen()} aria-expanded={this.state.open ? 'true' : 'false'} className="dropdown-toggle" data-toggle="dropdown">
+          <li onMouseLeave={ () => this.changeOpen() } className={`dropdown user user-menu ${this.state.open ? 'open' : ''}`}>
+            <a href="javascript:;" onClick={ () => this.changeOpen() } aria-expanded={ this.state.open ? 'true' : 'false' } className="dropdown-toggle" data-toggle="dropdown">
               <img src="http://lorempixel.com/160/160/abstract"
               className="user-image" alt="User Image" />
-              <span className="hidden-xs">{name}</span>
+              <span className="hidden-xs">{ name }</span>
             </a>
 
             <ul className="dropdown-menu">
               <li className="user-header">
                 <img src="http://lorempixel.com/160/160/abstract" className="img-circle" alt="User Image" />
-                <p>{name}<small>{email}</small></p>
+                <p>{ name }<small>{ email }</small></p>
               </li>
 
               <li className="user-footer">
                 <div className="pull-right">
-                  <a href="#" onClick={this.props.logout} className="btn btn-default btn-flat">Sair</a>
+                  <a href="#" onClick={ this.props.logout } className="btn btn-default btn-flat">Sair</a>
                 </div>
               </li>
             </ul>
